1226c3d9eb2afd8a1bf7aa3f609204063e633c70
Bryce Francis Tayler <brycetayler03@gmail.com>
2026-03-20 18:55:00 +0200

    refractor Server.java to SimpleServer.java, SimpleServer set up

---
+++ src/SimpleServer.java
@@ -0,0 +1,122 @@
+import java.io.*;
+import java.net.*;
+
+/**
+ * Simple server class.
+ * 
+ * @author Bryce Tayler
+ */
+public class Server {
+    private ServerSocket server;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private static final int PORT = 1234;
+    private static final String STOP_STRING = "##";
+    // private String serverLog;
+
+    public Server() {
+        try {
+            serverMessage("Attempting Server Start...");
+            server = new ServerSocket(PORT);
+
+            serverMessage("-----------------\nSUCCESSFUL START!\n-----------------");
+
+        } catch (Exception e) {
+            serverError("FAILED TO START SERVER!");
+
+            e.printStackTrace();
+
+            System.exit(0);
+
+        }
+
+        initConnections();
+
+    }
+
+    /**
+     * Get clients to join the server.
+     */
+    private void initConnections() {
+        serverMessage("Waiting for client to connect...");
+
+        try {
+            Socket clientSocket = server.accept();
+
+            in = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            serverMessage("Client Successfully connected!\n");
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            serverError("Failed to connect to client!");
+
+            e.printStackTrace();
+
+            System.exit(0);
+        }
+
+        // Read messages from client
+        readMessages();
+
+        // Close the server
+        close();
+
+    }
+
+    /**
+     * Close the server.
+     */
+    private void close() {
+        try {
+            in.close();
+            server.close();
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            serverError("Failed closing server!\n");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reads messages from client;
+     */
+    private void readMessages() {
+        try {
+            String line = "";
+            serverMessage("Beginning to read messages from client:\n");
+
+            while (!line.equals(STOP_STRING)) {
+
+                line = in.readUTF();
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Prints general server messages to console.
+     * 
+     * @param message - Server message.
+     */
+    private void serverMessage(String message) {
+        System.out.println("[SERVER MESSAGE]\t: \"+ message +\"");
+    }
+
+    /**
+     * Prints server error messages to console.
+     * 
+     * @param message - Error message.
+     */
+    private void serverError(String message) {
+        System.err.println("[SERVER ERROR]\t: \"+ message +\"\n");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
